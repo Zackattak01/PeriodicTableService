c3166fbb359399e565fdf5b08bbaecc9e7e66413
Zack Broderson <zbroderson@hotmail.com>
2021-03-03 10:49:52 -0500

    Restructure

--- PeriodicTableService/Models/PeriodicTable.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace PeriodicTableService
-{
-    public class PeriodicTable
-    {
-        public Element[] Elements { get; internal set; }
-    }
-}
---
+++ PeriodicTableServiceExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using PeriodicTableService;
+
+namespace PeriodicTableServiceExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(async (services) => await Peridtio)
+        }
+    }
+}
